d152d316a14a689b58fd13d3840f83a93d716bc4
MMQuants <mar4o86@mail.bg>
2020-12-05 10:35:47 +0200

    confidence intervals 95% with polinom ^3

---
+++ MATLAB solutions/confidence_intervals_95p_cubic_polynom_fit3.m
@@ -0,0 +1,27 @@
+% Raw data
+years = [1959 , 1960 , 1961 , 1962 , 1963 , 1964 , 1965 , 1966 , 1967 , 1968 , 1969];
+x = transpose(years);
+population = [4835 ,4970 , 5085 , 5160 , 5310 , 5260 , 5235 , 5255 , 5235 , 5210 , 5175];
+y = transpose(population);
+% Rescale years
+M = mean(x);
+xnorm = x - M;
+% Generate regression , construct confidence intervals
+p_fit = polyfit(xnorm,y , 3);
+%mdl = fitlm(xnorm, y, 3);
+%y_pred = predict(mdl,xnorm);
+
+beta0 = p_fit;
+modelfun = @(b,xnorm)(b(1)+b(2)*xnorm + b(3)*xnorm.^2+ b(4)*xnorm.^3);
+[beta,R,J,CovB,MSE] = nlinfit(xnorm,y,modelfun,beta0);
+[Ypred,delta] = nlpredci(modelfun,xnorm,beta,R,'Covar',CovB);
+lower = Ypred - delta;
+upper = Ypred + delta;
+lower_t = transpose(lower);
+upper_t = transpose(upper);
+%xrange = min(xnorm):.01:max(xnorm);
+figure()
+plot(xnorm,y,'ko') % observed data
+hold on
+plot(xnorm,Ypred,'k','LineWidth',1)
+plot(xnorm,[lower_t;upper_t],'r--','LineWidth',0.7)
